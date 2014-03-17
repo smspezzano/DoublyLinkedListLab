3816a57e6ddbb59a8bf97afd85f3aa6418d154f2
smspezzano <smspezzano@gmail.com>
2014-03-17 13:20:59 -0700

    got tests to pass for node.rb

--- node.rb
+++ node.rb
@@ -4,5 +4,31 @@ class Node
   
   def initialize(value)
     @value = value
+    @next = nil
+    @previous = nil
+  end 
+
+  def next=(next_node)
+    @next = next_node 
+  end
+
+  def previous=(previous)
+    @previous = previous
+  end
+
+  def next
+    @next
+  end
+
+  def previous
+    @previous
+  end
+  
+  def value=(value)
+    @value = value
+  end
+
+  def value
+    @value
   end
 end
